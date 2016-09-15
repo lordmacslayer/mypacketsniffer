6abb07c347652217258227b87a6f76d634d25791
lordmacslayer <slay.lordmac@gmail.com>
2016-09-16 01:20:33 +0530

    udp hex plus chars dump success

--- src/process_udp.c
+++ src/process_udp.c
@@ -16,6 +16,7 @@ void process_udp(const u_char *packet, struct ip *ip_packet, unsigned int ip_hea
 	struct udphdr *udp_header = (struct udphdr *) (packet + sizeof(struct ether_header) + sizeof(struct ip));
 	unsigned int payload_length;
 	unsigned char *payload_ptr = NULL;
+	unsigned char *dump_ascii = NULL;
 	int insert_new_line;
 
 	printf("\n%20s", "TRANSPORT LAYER");
@@ -29,19 +30,41 @@ void process_udp(const u_char *packet, struct ip *ip_packet, unsigned int ip_hea
 	// minimum length of the UDP packet has to be that of the header fields.
 	// so the minimum length has to be 8 bytes
 	payload_length = ntohs(udp_header->uh_ulen) - UDP_MIN_LEN;
-	payload_ptr = (unsigned char *)(udp_header + sizeof(struct udphdr));
+	payload_ptr = (unsigned char *)(udp_header + 1);
+	printf("\nDBG: Payload_ptr = %u\t\tudp_header = %u", (unsigned int *)payload_ptr, (unsigned char *)udp_header);
 	printf("\nUDP Payload (length = %u bytes) :", payload_length);
 	insert_new_line = 0;
-	// TODO: check if the payload is being properly printed by comparing with wireshark
+	dump_ascii = payload_ptr;
 	while (payload_length > 0)
 	{
 		if (insert_new_line % 8 == 0)
 		{
+			printf("\t\t");
+			// dump the ascii equivalent of the printed characters
+			while (dump_ascii != payload_ptr)
+			{
+				if (*dump_ascii >=32 && *dump_ascii <= 128)
+					printf("%c", (unsigned char)*dump_ascii);
+				else
+					printf(".");
+				dump_ascii++;
+			}
 			printf("\n\t");
 		}
-		printf("%02X ", (unsigned char)*payload_ptr);
+		printf("%02X ", (unsigned int)*payload_ptr);
 		payload_ptr++;
 		payload_length--;
 		insert_new_line = (insert_new_line + 1) % 8;
+	}
+	// this while loop is to dump the ascii for the last line hex dump
+	printf("%*c",23-insert_new_line-(insert_new_line - 1),' ');
+	printf("\t\t");
+	while (dump_ascii != payload_ptr)
+	{
+		if (*dump_ascii >=32 && *dump_ascii <= 128)
+			printf("%c", (unsigned char)*dump_ascii);
+		else
+			printf(".");
+		dump_ascii++;
 	}
 }
